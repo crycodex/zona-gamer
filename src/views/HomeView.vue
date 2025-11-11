8eb2199a16a0c55ef877652b2d2c88604e2849d1
crycodex <recaldecd@gmail.com>
2025-11-10 19:30:25 -0500

    clean repo and add tailwind 4.1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Zona Gamer</h1>
+    <p>Welcome to the Zona Gamer</p>
+    <p>We are a team of gamers who are passionate about gaming</p>
   </main>
 </template>
